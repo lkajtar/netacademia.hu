8186bb929b79cf6f9e5c02270d3c8427be7ce35c
puffy <nagy.gergo@3pixel.hu>
2018-04-03 10:20:42 +0200

    ajjaj

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        NetAcadmeia TravisCI setup test
+        NetAcadmeia TravisCI git test
       </h1>
       <h2 class="subtitle">
         Nuxt.js project
