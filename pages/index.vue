3409de5fd250dc3ccf406033e8770180e9cca1be
puffy <nagy.gergo@3pixel.hu>
2018-04-07 15:52:58 +0200

    landing integracio kesz
    kb teljesen újra kell majd építeni később a oldalt

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,340 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        NetAcadmeia TravisCI git test Gabinak2
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <main class="fullscreen-bg">
+
+    <video loop muted autoplay
+           poster="/background.jpg"
+           class="fullscreen-bg__video">
+      <source
+        src="https://player.vimeo.com/external/214413686.hd.mp4?s=20bc6888428db4eff09a3dff53d6ac73d75dd2ab&profile_id=174">
+    </video>
+
+
+    <img class="logocska" src="/logo.png"
+         alt="NetAcademia Oktatóközpont Kft.">
+    <div id="headerbox">
+      <ol>
+        <li><a href="/tanfolyamkeresov2">Tanfolyamkereső</a></li>
+        <li><a href="/certificate" class="hideonmobile">Certificate</a></li>
+        <li><a href="/lenyugozo-tanfolyamok" class="hideonmobile">
+          Lenyűgöző Tanfolyamok
+        </a></li>
+        <li><a href="/kapcsolat" class="hideonmobile">Kapcsolat</a></li>
+        <li><a href="/Adataim">Adataim</a></li>
+      </ol>
+    </div>
+
+
+    <div class="vfkozepre">
+      <div class="cim">Élő, online oktatás bárhol, bármikor</div>
+      <div class="gombdoboz">
+        <a href="/indulotanfolyamok">
+          <div class="gomb">Induló tanfolyamok</div>
+        </a>
+        <a href="/ultimateakcio">
+          <div class="akciogomb">Ultimate Akció!</div>
+        </a>
+        <a href="/oktato">
+          <div class="gomb">Jelentkezz oktatónak!</div>
+        </a>
+        <a href="/webfejleszto-vuejs">
+          <div class="gomb">Webfejlesztő Vue.js</div>
+        </a>
+      </div>
+      <br/>
+      <div class="hirlev">
+        <a href="https://netacademia.hu/www.netacademia.hu_NewsLetter">
+          Hírlevél-feliratkozás
+        </a>
       </div>
     </div>
-  </section>
+
+
+    <div class="lablec vkozepre">
+      © NetAcademia 1998-{{ currentYear }}
+      <span>
+        <a href="GeneralTermsAndConditions/Subscription">ÁSZF</a>
+      </span>
+      <span>
+        <a href="GeneralTermsAndConditions/PrivacyStatement">Adatvédelem</a>
+      </span>
+    </div>
+  </main>
 </template>
 
 <script>
-import AppLogo from "~/components/AppLogo.vue";
-
-export default {
-  components: {
-    AppLogo
-  }
-};
+  export default {
+    computed: {
+      currentYear: function () {
+        return new Date().getFullYear();
+      }
+    }
+  };
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  @import url('https://fonts.googleapis.com/css?family=Work+Sans:200,300,400,500,600,700&subset=latin-ext');
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  html, body {
+    margin: 0;
+    overflow: hidden;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  main {
+    height: 100vh;
+    padding: 0;
+    font-family: "Work sans", Arial;
+    color: white;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  ol {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  ol li {
+    display: inline;
+  }
+
+  .logocska {
+    position: absolute;
+    margin-top: 20px;
+    margin-left: 20px;
+    width: 50px;
+    height: 50px;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  .gombdoboz a {
+    margin: 10px;
+    display: inline-block;
+  }
+
+  .cim {
+    font-size: 50px;
+    font-weight: 300;
+  }
+
+  .vkozepre {
+    position: absolute;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, 0);
+  }
+
+  .vfkozepre {
+    position: absolute;
+    top: 50%;
+    margin-bottom: -50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+
+  #headerbox {
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, 0);
+    font-family: Arial;
+    text-transform: uppercase;
+    font-size: 14px;
+    margin-top: 1px;
+  }
+
+  #headerbox a {
+    margin: 0px 10px;
+    text-decoration: none;
+  }
+
+  #headerbox a:hover {
+    color: rgb(203, 203, 203);
+    transition-duration: 0.5s;
+  }
+
+  .lablec {
+    font-family: Arial;
+    position: absolute;
+    bottom: 0;
+    font-size: 14px;
+    margin-bottom: 5px;
+    text-align: center;
+  }
+
+  .lablec a {
+    margin: 0px 10px;
+    text-decoration: none;
+  }
+
+  .lablec a:hover {
+    color: rgb(203, 203, 203);
+    transition-duration: 0.5s;
+  }
+
+  .gombdoboz {
+    margin-top: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    display: table;
+  }
+
+  .gomb {
+    padding: 12px;
+    width: 165px;
+    background: rgba(0, 0, 0, 0.5);
+    font-family: Arial;
+    font-size: 12px;
+    font-weight: 600;
+    text-transform: uppercase;
+    display: table-cell;
+    vertical-align: middle;
+    height: 18px;
+    border: 1px solid transparent;
+
+  }
+
+  .gomb:hover {
+    border: 1px solid white;
+    transition-duration: 0.5s;
+  }
+
+  .akciogomb {
+    padding: 12px;
+    width: 165px;
+    background: rgba(255, 0, 0, 0.6);
+    font-family: Arial;
+    font-size: 12px;
+    font-weight: 600;
+    text-transform: uppercase;
+    display: table-cell;
+    vertical-align: middle;
+    height: 18px;
+    border: 1px solid transparent;
+
+  }
+
+  .akciogomb:hover {
+    border: 1px solid white;
+    transition-duration: 0.5s;
+  }
+
+  .hirlev {
+    margin-top: 30px;
+    text-decoration: none;
+    font-family: Arial;
+    display: inline-block;
+  }
+
+  .hirlev a:hover {
+    color: rgb(203, 203, 203);
+    transition-duration: 0.5s;
+  }
+
+  #conference {
+    margin-top: 30px;
+    text-decoration: none;
+    font-family: Arial;
+    display: inline-block;
+  }
+
+  #conference a:hover {
+    color: rgb(203, 203, 203);
+    transition-duration: 0.5s;
+  }
+
+  .fullscreen-bg {
+    z-index: -100;
+    background: url('/background.jpg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .fullscreen-bg__video {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: auto;
+    height: auto;
+    min-width: 100%;
+    min-height: 100%;
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    pointer-events: none;
+  }
+
+  #year {
+    margin: 0 10px 0 0;
+  }
+
+  @media (max-width: 767px) {
+
+    .fullscreen-bg__video {
+      display: none;
+    }
+
+    .logocska {
+      display: none;
+    }
+
+    .hideonmobile {
+      display: none;
+    }
+
+    #headerbox {
+      font-size: 12px;
+      width: 100%;
+    }
+
+    ol {
+      padding: 0px;
+    }
+
+    #headerbox a {
+      text-align: center;
+    }
+
+    .cim {
+      font-size: 35px;
+    }
+
+    .lablec {
+      margin-left: 5px;
+    }
+
+    #conference {
+      margin-top: 12px;
+    }
+
+    .gombdoboz {
+      margin-top: 8px;
+    }
+  }
+
+  @media (max-width: 340px) {
+    .lablec {
+      font-size: 12px;
+    }
+  }
+
+  @media (max-height: 450px) {
+    #headerbox {
+      display: none;
+    }
+
+    .lablec {
+      display: none;
+    }
+
+    .hirlev {
+      margin-top: 20px;
+    }
+  }
 </style>
